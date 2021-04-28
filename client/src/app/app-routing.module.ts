c9e2610ed7d9e3dc11d80e83b00ddae2494a7d7c
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-04-28 21:31:22 +0200

    add fallback route

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { CurrentTempComponent } from './components/current-temp/current-temp.component';
 
 const routes: Routes = [
-  {path: '', component: CurrentTempComponent}
+  {path: '', component: CurrentTempComponent},
+  {path: '**', component: CurrentTempComponent}
 ];
 
 @NgModule({
