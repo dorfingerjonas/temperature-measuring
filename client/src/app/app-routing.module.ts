6fd6c74838cd00421e49c3208be71ae5da7a93a4
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-04-28 21:31:00 +0200

    add routing for default path

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CurrentTempComponent } from './components/current-temp/current-temp.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CurrentTempComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
