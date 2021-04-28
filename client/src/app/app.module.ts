5d0b84e00cd51b61cc3873908fc547a5e1515129
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-04-28 21:41:53 +0200

    import firebase modules

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CurrentTempComponent } from './components/current-temp/current-temp.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { CurrentTempComponent } from './components/current-temp/current-temp.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig, 'temperature'),
+    AngularFireDatabaseModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
