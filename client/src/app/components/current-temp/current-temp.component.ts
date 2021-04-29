aebef0813842c857963b109d024c5ef72c62e34c
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-04-29 09:36:21 +0200

    implement formatDate()

--- client/src/app/components/current-temp/current-temp.component.ts
+++ client/src/app/components/current-temp/current-temp.component.ts
@@ -12,4 +12,15 @@ export class CurrentTempComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  formatDate(date: Date): string {
+    const year = new Intl.DateTimeFormat('de', {year: 'numeric'}).format(date);
+    const month = new Intl.DateTimeFormat('de', {month: 'short'}).format(date);
+    const day = new Intl.DateTimeFormat('de', {day: '2-digit'}).format(date);
+    const weekDay = new Intl.DateTimeFormat('de', {weekday: 'short'}).format(date);
+    const hours = ('0' + date.getHours()).slice(-2);
+    const minutes = ('0' + date.getMinutes()).slice(-2);
+    const seconds = ('0' + date.getSeconds()).slice(-2);
+
+    return `${ weekDay }, ${ day }. ${ month } ${ year } ${ hours }:${ minutes }:${ seconds }`;
+  }
 }
