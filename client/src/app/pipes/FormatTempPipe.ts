40351b81da4304a4b97e632cc5bbf386dd7a2fab
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-04-29 21:03:30 +0200

    modify FormatTempPipe.ts

--- client/src/app/pipes/FormatTempPipe.ts
+++ client/src/app/pipes/FormatTempPipe.ts
@@ -5,12 +5,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FormatTemperaturePipe implements PipeTransform {
   transform(temperature: number): string {
-    const tempParts: string[] = temperature.toString().split('.');
-
-    if (tempParts.length > 1) {
-      return `${ tempParts[0] },${ Math.round(parseFloat(tempParts[1]) / 100) }`;
-    } else {
-      return `${ temperature },0`;
-    }
+    return temperature.toString().replace('.', ',');
   }
 }
