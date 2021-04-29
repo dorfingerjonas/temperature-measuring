8e0b172f16b75cebff57cebfd4bf6c983f851211
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-04-29 09:38:40 +0200

    implement /history routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CurrentTempComponent } from './components/current-temp/current-temp.component';
+import { HistoryComponent } from './components/history/history.component';
 
 const routes: Routes = [
   {path: '', component: CurrentTempComponent},
+  {path: 'history', component: HistoryComponent},
   {path: '**', redirectTo: ''}
 ];
 
