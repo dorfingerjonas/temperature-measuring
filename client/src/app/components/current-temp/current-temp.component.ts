f948d843abc54158770ac8b8de347bf8c9f09d7a
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-04-29 21:04:42 +0200

    remove complex structure

--- client/src/app/components/current-temp/current-temp.component.ts
+++ client/src/app/components/current-temp/current-temp.component.ts
@@ -9,7 +9,7 @@ import { AnimationOptions } from 'ngx-lottie';
 })
 export class CurrentTempComponent implements OnInit {
 
-  temperature?: string;
+  temperature?: number;
   time?: string;
   showLoading = true;
   options: AnimationOptions = {
@@ -24,29 +24,10 @@ export class CurrentTempComponent implements OnInit {
       this.showLoading = false;
       const measuring: any = value;
 
-      if (measuring.temp && measuring.timestamp) {
-        const tempParts: string[] = measuring.temp.toString().split('.');
-
-        if (tempParts.length > 1) {
-          this.temperature = `${ tempParts[0] },${ Math.round(parseFloat(tempParts[1]) / 100) }`;
-        } else {
-          this.temperature = `${ measuring.temp },0`;
-        }
-
-        this.time = `${ this.formatDate(new Date(parseInt(measuring.timestamp, 0))) }`;
+      if (measuring.temperature && measuring.timestamp) {
+        this.temperature = measuring.temperature;
+        this.time = measuring.timestamp;
       }
     });
-  }
-
-  formatDate(date: Date): string {
-    const year = new Intl.DateTimeFormat('de', {year: 'numeric'}).format(date);
-    const month = new Intl.DateTimeFormat('de', {month: 'short'}).format(date);
-    const day = new Intl.DateTimeFormat('de', {day: '2-digit'}).format(date);
-    const weekDay = new Intl.DateTimeFormat('de', {weekday: 'short'}).format(date);
-    const hours = ('0' + date.getHours()).slice(-2);
-    const minutes = ('0' + date.getMinutes()).slice(-2);
-    const seconds = ('0' + date.getSeconds()).slice(-2);
-
-    return `${ weekDay }, ${ day }. ${ month } ${ year } ${ hours }:${ minutes }:${ seconds }`;
   }
 }
