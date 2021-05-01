4d86412a7eb09c5fcbb96764b962d0deaa3272fb
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-05-01 13:29:49 +0200

    add settings route

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CurrentTempComponent } from './components/current-temp/current-temp.component';
 import { HistoryComponent } from './components/history/history.component';
+import { SettingsComponent } from "./components/settings/settings.component";
 
 const routes: Routes = [
   {path: '', component: CurrentTempComponent},
   {path: 'history', component: HistoryComponent},
+  {path: 'settings', component: SettingsComponent},
   {path: '**', redirectTo: ''}
 ];
 
