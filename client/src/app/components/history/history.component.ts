024e904e9f6ed3ff924179d1f318e9cdef2faf8b
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-05-03 22:46:40 +0200

    implement currentDay chart

--- client/src/app/components/history/history.component.ts
+++ client/src/app/components/history/history.component.ts
@@ -3,6 +3,7 @@ import { AngularFireDatabase } from '@angular/fire/database';
 import Measuring from '../../model/measuring';
 import { AnimationOptions } from 'ngx-lottie';
 import { Location } from '@angular/common';
+import { HistoryDataService } from "../../services/history-data.service";
 
 @Component({
   selector: 'app-history',
@@ -11,14 +12,16 @@ import { Location } from '@angular/common';
 })
 export class HistoryComponent implements OnInit {
 
-  measures: Measuring[] = [];
+  private measures: Measuring[] = [];
   showLoading = true;
+  currentDate?: string;
   options: AnimationOptions = {
     path: 'assets/animations/thermometer.json'
   };
 
   constructor(private db: AngularFireDatabase,
-              private location: Location) {
+              private location: Location,
+              private historyDataService: HistoryDataService) {
   }
 
   ngOnInit(): void {
@@ -33,6 +36,30 @@ export class HistoryComponent implements OnInit {
         }
       }
     });
+
+    const date = new Date();
+
+    const year = new Intl.DateTimeFormat('de', {year: 'numeric'}).format(date);
+    const month = new Intl.DateTimeFormat('de', {month: 'short'}).format(date);
+    const day = new Intl.DateTimeFormat('de', {day: '2-digit'}).format(date);
+
+    this.currentDate = `${ day }. ${ month } ${ year }`;
+  }
+
+  getMeasuresOfCurrentDay(): Measuring[] {
+    const measures = this.historyDataService.getMeasuresOfCurrentDay(this.measures);
+
+    measures.forEach(m => {
+      const date = new Date(parseInt(m.timestamp.toString(), 0));
+
+      const hours = ('0' + date.getHours()).slice(-2);
+      const minutes = ('0' + date.getMinutes()).slice(-2);
+      const seconds = ('0' + date.getSeconds()).slice(-2);
+
+      m.label = `${ hours }:${ minutes }:${ seconds }`;
+    });
+
+    return measures;
   }
 
   back(): void {
