21116f818f0a7ceb317e4948764eed7c254d390d
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-05-04 15:10:13 +0200

    implement history-data service

--- client/src/app/services/history-data.service.ts
+++ client/src/app/services/history-data.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import Measuring from '../model/measuring';
+import { JsonObject } from "@angular/compiler-cli/ngcc/src/packages/entry_point";
+import ChartData from "../model/chart-data";
 
 @Injectable({
   providedIn: 'root'
@@ -9,20 +11,61 @@ export class HistoryDataService {
   constructor() {
   }
 
-  getMeasuresOfCurrentDay(measures: Measuring[]): Measuring[] {
-    const measuresOfCurrentDay: Measuring[] = []
-    const currentTime = new Date();
+  getMeasureByDay(measures: Measuring[], date: Date): ChartData[] {
+    const measuresOfCurrentDay: ChartData[] = []
 
     measures.forEach(m => {
       const measureDate = new Date(m.timestamp);
 
-      if (measureDate.getDay() === currentTime.getDay()
-        && measureDate.getMonth() === currentTime.getMonth()
-        && measureDate.getFullYear() === currentTime.getFullYear()) {
-        measuresOfCurrentDay.push(m);
+      if (measureDate.getDay() === date.getDay()
+        && measureDate.getMonth() === date.getMonth()
+        && measureDate.getFullYear() === date.getFullYear()) {
+
+        const hours = ('0' + measureDate.getHours()).slice(-2);
+        const minutes = ('0' + measureDate.getMinutes()).slice(-2);
+        const seconds = ('0' + measureDate.getSeconds()).slice(-2);
+
+        measuresOfCurrentDay.push({
+          time: `${ hours }:${ minutes }:${ seconds }`,
+          temperature: m.temperature
+        })
       }
     });
 
     return measuresOfCurrentDay;
+  }
+
+  getMaxTemperaturePerDay(measures: Measuring[]): ChartData[] {
+    const days: string[] = this.getDaysWhereDataExists(measures);
+    const result: ChartData[] = [];
+
+    days.forEach(day => {
+      result.push({
+        time: day,
+        temperature: Math.max(...measures.filter(m => m.label === day).map(m => m.temperature))
+      });
+    });
+
+    return result;
+  }
+
+  getDaysWhereDataExists(measures: Measuring[]): string[] {
+    const days: string[] = [];
+
+    measures.forEach(m => {
+      const measureDate = new Date(m.timestamp);
+
+      const year = new Intl.DateTimeFormat('de', {year: 'numeric'}).format(measureDate);
+      const month = new Intl.DateTimeFormat('de', {month: 'short'}).format(measureDate);
+      const day = new Intl.DateTimeFormat('de', {day: '2-digit'}).format(measureDate);
+
+      m.label = `${ day }. ${ month } ${ year }`
+
+      if (!days.includes(m.label)) {
+        days.push(m.label);
+      }
+    });
+
+    return days;
   }
 }
