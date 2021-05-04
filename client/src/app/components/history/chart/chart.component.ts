7d8da9978ba6e580b03d37eaea2974a53378bee1
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-05-04 15:10:48 +0200

    implement chart.component.ts

--- client/src/app/components/history/chart/chart.component.ts
+++ client/src/app/components/history/chart/chart.component.ts
@@ -1,32 +1,47 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import ChartData from "../../../model/chart-data";
 import { Chart } from "chart.js";
-import Measuring from "../../../model/measuring";
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.scss']
 })
-export class ChartComponent implements OnInit {
+export class ChartComponent implements OnInit, OnChanges {
 
+  @Input() id?: string;
   @Input() name?: string;
-  @Input() measures: Measuring[] = [];
+  @Input() measures: ChartData[] = [];
 
   constructor() {
   }
 
-  ngOnInit(): void {
-    if (this.name) {
-      new Chart('chart', {
-        type: 'line',
-        data: {
-          labels: this.measures.map(m => m.label || ''),
-          datasets: [{
-            label: 'Temperatur',
-            data: this.measures.map(m => m.temperature),
-            fill: false,
-            borderColor: 'rgb(25,118,210)'
-          }]
+  ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.measures) {
+      setTimeout(() => {
+        if (this.id) {
+          new Chart(this.id, {
+            type: 'line',
+            data: {
+              labels: this.measures.map(m => m.time),
+              datasets: [{
+                label: 'Temperatur',
+                data: this.measures.map(m => m.temperature),
+                fill: false,
+                borderColor: 'rgb(25, 118, 210)'
+              }]
+            },
+            options: {
+              tooltips: {
+                callbacks: {
+                  label: i => `${ i.yLabel } °C`
+                }
+              }
+            }
+          });
         }
       });
     }
